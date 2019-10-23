a52e6d55d1449b2cc63db4f15899fa8de0bc4486
himanshu <rathihimanshu@live.com>
2019-10-23 16:45:47 +0530

    new one

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo apt-get install -y tomcat7
-chmod -R 777 /var/lib/tomcat7
-cp target/JenkinsWar.war /var/lib/tomcat7/webapps/
+#sudo apt-get install -y tomcat7
+#chmod -R 777 /var/lib/tomcat7
+#cp target/JenkinsWar.war /var/lib/tomcat7/webapps/
